986f7ccab5e8b7fa8531a5749ed8702a702f38b5
root <root@vps-b725e1b1.vps.ovh.ca>
2026-07-11 10:24:17 +0000

    Add professional copyright headers to all Java source files

--- src/main/java/dev/ajaretro/retroworldpurger/RetroWorldPurger.java
+++ src/main/java/dev/ajaretro/retroworldpurger/RetroWorldPurger.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2026 AJA_RETRO (https://ajaretro.dev). All Rights Reserved.
+ * 
+ * This source code and compiled binaries are the intellectual property of the author.
+ * Redistribution, modification, or derivative works are strictly prohibited under the
+ * terms of the Source-Available License.
+ */
+
 package dev.ajaretro.retroworldpurger;
 
 import org.bstats.bukkit.Metrics;
